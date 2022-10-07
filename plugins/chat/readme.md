fe01cd9b9f194c04f87a8574971c546755652900
tong <lbuchiyu@gmail.com>
2022-10-07 23:01:06 +0800

    init: first commit

---
+++ plugins/chat/readme.md
@@ -0,0 +1,5 @@
+# koishi-plugin-chat
+
+[![npm](https://img.shields.io/npm/v/koishi-plugin-chat?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-chat)
+
+chat desc
---
+++ plugins/chat/src/index.ts
@@ -0,0 +1,27 @@
+import { Context, Schema, Session } from 'koishi';
+import { sample as _sample } from 'lodash';
+import '@koishijs/plugin-adapter-onebot';
+
+export const name = 'chat';
+
+export interface Config {}
+
+export const Config: Schema<Config> = Schema.object({});
+
+export function apply(ctx: Context) {
+  ctx.guild(...['123','#']).middleware(async (session: Session, next) => {
+    const {
+      bot,
+      content,
+      guildId,
+      author: { userId, username },
+      onebot,
+    } = session;
+    const { last_sent_time } = await onebot?.getGroupMemberInfo(guildId, userId);
+    console.log('🚀~ 50  ');
+    // if () {
+    //
+    // }
+    await next();
+  });
+}
